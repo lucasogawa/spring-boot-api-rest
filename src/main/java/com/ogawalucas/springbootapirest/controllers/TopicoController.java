35a9c77e267be8989513eba9d2d7b79ebcbd1342
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-14 22:54:15 -0300

    Adding personal query with Spring Data JPA

--- src/main/java/com/ogawalucas/springbootapirest/controllers/TopicoController.java
+++ src/main/java/com/ogawalucas/springbootapirest/controllers/TopicoController.java
@@ -1,14 +1,11 @@
 package com.ogawalucas.springbootapirest.controllers;
 
 import com.ogawalucas.springbootapirest.dtos.TopicoDto;
-import com.ogawalucas.springbootapirest.models.Curso;
-import com.ogawalucas.springbootapirest.models.Topico;
 import com.ogawalucas.springbootapirest.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,7 +15,11 @@ public class TopicoController {
     private TopicoRepository repository;
 
     @RequestMapping("topicos")
-    public List<TopicoDto> lista() {
-        return TopicoDto.converter(repository.findAll());
+    public List<TopicoDto> lista(String cursoNome) {
+        if (cursoNome == null) {
+            return TopicoDto.converter(repository.findAll());
+        } else {
+            return TopicoDto.converter(repository.findByCursoNome(cursoNome));
+        }
     }
 }
--- src/main/java/com/ogawalucas/springbootapirest/repositories/TopicoRepository.java
+++ src/main/java/com/ogawalucas/springbootapirest/repositories/TopicoRepository.java
@@ -3,6 +3,9 @@ package com.ogawalucas.springbootapirest.repositories;
 import com.ogawalucas.springbootapirest.models.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+    List<Topico> findByCursoNome(String cursoNome);
 }

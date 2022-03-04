84852c921c0b6a8742786cb1d38bdf89bc1dccd9
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-03-03 23:06:24 -0300

    Adding @CacheEvict

--- src/main/java/com/ogawalucas/springbootapirest/controllers/TopicoController.java
+++ src/main/java/com/ogawalucas/springbootapirest/controllers/TopicoController.java
@@ -8,6 +8,7 @@ import com.ogawalucas.springbootapirest.models.Topico;
 import com.ogawalucas.springbootapirest.repositories.CursoRepository;
 import com.ogawalucas.springbootapirest.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -43,6 +44,7 @@ public class TopicoController {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "lista", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         repository.save(topico);
@@ -60,6 +62,7 @@ public class TopicoController {
 
     @Transactional
     @PutMapping("{id}")
+    @CacheEvict(value = "lista", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         return repository.findById(id)
             .map(topico -> ResponseEntity.ok(new TopicoDto(form.atualizar(topico))))
@@ -68,6 +71,7 @@ public class TopicoController {
 
     @Transactional
     @DeleteMapping("{id}")
+    @CacheEvict(value = "lista", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
         var topico = repository.findById(id);
 

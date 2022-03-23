889786a6ef21aecc750aa5d793343e0e025b5b3b
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-03-22 21:45:10 -0300

    Adding Configuration of HttpRequests

--- src/main/java/com/ogawalucas/springbootapirest/config/security/SecurityConfigurations.java
+++ src/main/java/com/ogawalucas/springbootapirest/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package com.ogawalucas.springbootapirest.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+            .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+            .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 }
